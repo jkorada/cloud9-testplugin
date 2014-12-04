aa7a00627453aab609447ac7f120ea44221799f7
Janardhan Korada <srikar2642@gmail.com>
2014-12-04 15:33:09 +0530

    Create testplugin.xml.js

---
+++ testplugin.xml.js
@@ -0,0 +1,28 @@
+// Wrapped in JavaScript, to avoid cross-origin restrictions, created using wrap-in-js.sh
+define(function() {
+return '<a:application xmlns:a="http://ajax.org/2005/aml">\n' +
+'    <a:window\n' +
+'      id        = "winTestPlugin"\n' +
+'      title     = "Test Plugin Window"\n' +
+'      center    = "true"\n' +
+'      modal     = "false"\n' +
+'      buttons   = "close"\n' +
+'      skin      = "bk-window"\n' +
+'      kbclose   = "true"\n' +
+'      class     = "relative"\n' +
+'      width     = "300">\n' +
+'        <a:vbox>\n' +
+'            <a:hbox padding="5" edge="10">\n' +
+'                Hello World!\n' +
+'            </a:hbox>\n' +
+'            <a:hbox pack="end" padding="5" edge="6 10 10">\n' +
+'                 <a:button \n' +
+'                  id="btnClose"\n' +
+'                  caption = "Close" \n' +
+'                  skin    = "btn-default-css3"\n' +
+'                 />\n' +
+'            </a:hbox>\n' +
+'        </a:vbox>\n' +
+'    </a:window>\n' +
+'</a:application>\n' +
+'';});
